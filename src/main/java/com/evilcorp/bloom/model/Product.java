52b0548a5a98b625898a028e868920fa8c1e6656
jess <70455379+jesslourenco@users.noreply.github.com>
2024-11-28 19:33:07 -0300

    product files working (no controller yet)

---
+++ src/main/java/com/evilcorp/bloom/model/Product.java
@@ -0,0 +1,116 @@
+package com.evilcorp.bloom.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("catalog")
+public class Product {
+  @Id
+  private Integer id;
+
+  @Column("product_name")
+  private String name;
+
+  private String description;
+  private Integer categoryId;
+  private Integer brandId;
+  private String imgUrl;
+
+  private Double price;
+  private Double cost;
+  private Integer stockQty;
+
+  public Product(Integer id, String name, String description, Integer categoryId, Integer brandId,
+      String imgUrl, Double price, Double cost, Integer stockQty) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.categoryId = categoryId;
+    this.brandId = brandId;
+    this.imgUrl = imgUrl;
+    this.price = price;
+    this.cost = cost;
+    this.stockQty = stockQty;
+  }
+
+  public Product(String name, String description, Integer categoryId, Integer brandId,
+      String imgUrl, Double price, Double cost, Integer stockQty) {
+    this(null, name, description, categoryId, brandId, imgUrl, price, cost, stockQty);
+  }
+
+  public Product() {
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Integer getCategoryId() {
+    return categoryId;
+  }
+
+  public void setCategoryId(Integer categoryId) {
+    this.categoryId = categoryId;
+  }
+
+  public Integer getBrandId() {
+    return brandId;
+  }
+
+  public void setBrandId(Integer brandId) {
+    this.brandId = brandId;
+  }
+
+  public String getImgUrl() {
+    return imgUrl;
+  }
+
+  public void setImgUrl(String imgUrl) {
+    this.imgUrl = imgUrl;
+  }
+
+  public Double getPrice() {
+    return price;
+  }
+
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+
+  public Double getCost() {
+    return cost;
+  }
+
+  public void setCost(Double cost) {
+    this.cost = cost;
+  }
+
+  public Integer getStockQty() {
+    return stockQty;
+  }
+
+  public void setStockQty(Integer stockQty) {
+    this.stockQty = stockQty;
+  }
+}
---
+++ src/test/java/com/evilcorp/bloom/service/ProductServiceTests.java
@@ -0,0 +1,193 @@
+package com.evilcorp.bloom.service;
+
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import com.evilcorp.bloom.dto.ProductDto;
+import com.evilcorp.bloom.dto.ProductFilterDto;
+import com.evilcorp.bloom.dto.ProductMapper;
+import com.evilcorp.bloom.model.Product;
+import com.evilcorp.bloom.model.Category;
+import com.evilcorp.bloom.repo.ProductRepo;
+import com.evilcorp.bloom.exception.NotFoundException;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceTests {
+  @Mock
+  private ProductRepo productRepo;
+
+  @InjectMocks
+  private ProductService productService;
+
+  @Spy
+  private ProductMapper productMapper;
+
+  private Product product;
+  private ProductDto dto;
+
+  @BeforeEach
+  public void init() {
+    this.dto = new ProductDto();
+    this.dto.setName("macbook air");
+    this.dto.setDescription("a laptop for the casual user");
+    this.dto.setCategoryId(null);
+    this.dto.setBrandId(1);
+    this.dto.setPrice(3000.67);
+    this.dto.setCost(2000.00);
+    this.dto.setStockQty(0);
+
+    product = new Product("Macbook Air",
+        "a laptop for the casual user",
+        null,
+        1,
+        null,
+        3000.67,
+        2000.00,
+        0);
+  }
+
+  @Test
+  public void testAdd() {
+    when(productMapper.toProduct(dto)).thenReturn(product);
+
+    productService.add(dto);
+
+    verify(productRepo, times(1))
+        .save(argThat(p -> p.getName().equals(product.getName()) &&
+            p.getDescription().equals(product.getDescription()) &&
+            p.getCategoryId() == null &&
+            p.getBrandId().equals(product.getBrandId()) &&
+            p.getPrice().equals(product.getPrice()) &&
+            p.getCost().equals(product.getCost()) &&
+            p.getStockQty().equals(product.getStockQty())));
+  }
+
+  @Test
+  public void testAdd_InvalidBrandId() {
+    when(productMapper.toProduct(dto)).thenReturn(product);
+    when(productRepo.save(any(Product.class)))
+        .thenThrow(new DataIntegrityViolationException("constraint [23503]: foreign key issue"));
+
+    assertThatThrownBy(() -> productService.add(dto))
+        .isInstanceOf(NotFoundException.class);
+  }
+
+  @Test
+  public void testUpdate() {
+    Integer id = 3;
+    when(productRepo.existsById(id)).thenReturn(true);
+    when(productMapper.toProduct(dto)).thenReturn(product);
+
+    productService.update(dto, id);
+
+    verify(productRepo, times(1))
+        .save(argThat(p -> p.getName().equals(product.getName()) &&
+            p.getDescription().equals(product.getDescription()) &&
+            p.getCategoryId() == null &&
+            p.getBrandId().equals(product.getBrandId()) &&
+            p.getPrice().equals(product.getPrice()) &&
+            p.getCost().equals(product.getCost()) &&
+            p.getStockQty().equals(product.getStockQty())));
+  }
+
+  @Test
+  public void testUpdate_InvalidProductId() {
+    Integer id = 2;
+    when(productRepo.existsById(id)).thenReturn(false);
+
+    assertThatThrownBy(() -> productService.update(dto, id))
+        .isInstanceOf(NotFoundException.class);
+  }
+
+  @Test
+  public void testUpdate_InvalidBrandId() {
+    Integer id = 2;
+    when(productRepo.existsById(id)).thenReturn(true);
+
+    when(productMapper.toProduct(dto)).thenReturn(product);
+    when(productRepo.save(any(Product.class)))
+        .thenThrow(new DataIntegrityViolationException("constraint [23503]: foreign key issue"));
+
+    assertThatThrownBy(() -> productService.update(dto, id))
+        .isInstanceOf(NotFoundException.class);
+  }
+
+  @Test
+  public void testFindAllByCategoryId() {
+    Category category = new Category("Electronics", null);
+    category.setId(1);
+
+    product.setCategoryId(1);
+
+    List<Product> products = new ArrayList<>();
+    products.add(product);
+
+    when(productRepo.findAllPaged(1, 10, 0)).thenReturn(products);
+    when(productRepo.countByCategory(1)).thenReturn((long) 1);
+
+    ProductFilterDto dto = new ProductFilterDto();
+    dto.page = 0;
+    dto.categoryId = 1;
+
+    productService.findAllByCategoryId(dto);
+
+    assertThat(products).hasSize(1);
+    assertThat(products).containsExactlyElementsOf(products);
+  }
+
+  @Test
+  public void testFindById() {
+    when(productRepo.findById(1)).thenReturn(Optional.of(product));
+
+    Product p = productService.findById(1);
+
+    assertTrue(
+        p.getName().equals(product.getName()) &&
+            p.getDescription().equals(product.getDescription()) &&
+            p.getCategoryId() == null &&
+            p.getBrandId().equals(product.getBrandId()) &&
+            p.getPrice().equals(product.getPrice()) &&
+            p.getCost().equals(product.getCost()) &&
+            p.getStockQty().equals(product.getStockQty()));
+  }
+
+  @Test
+  public void testFindById_NotFound() {
+    when(productRepo.findById(1)).thenThrow(new NotFoundException("not found"));
+
+    assertThatThrownBy(() -> productService.findById(1))
+        .isInstanceOf(NotFoundException.class);
+  }
+
+  @Test
+  public void testDeleteById() {
+    doNothing().when(productRepo).deleteById(1);
+
+    productService.deleteById(1);
+    verify(productRepo, times(1)).deleteById(1);
+  }
+
+  @Test
+  public void testDeleteById_NotFound() {
+    doThrow(new EmptyResultDataAccessException(1)).when(productRepo).deleteById(1);
+    assertThatThrownBy(() -> productService.deleteById(1))
+        .isInstanceOf(NotFoundException.class);
+  }
+
+}
